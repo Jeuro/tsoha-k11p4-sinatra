fd97fcb2e52f18ff5216ce3b53cfccf2473bb07b
Eeva Terkki <juuuro@gmail.com>
2011-03-30 23:52:48 +0300

    navigaatio

--- tsoha.rb
+++ tsoha.rb
@@ -1,26 +1,42 @@
+﻿# encoding: utf-8
+
 require 'rubygems'
 require 'sinatra'
 require 'erb'
 
-require 'config/init'
+require './config/init'
 
-require 'models/user'
+require './models/user'
 
-class Tsoha < Sinatra::Base
 
-  enable :sessions
-  set :public, File.dirname(__FILE__) + "/public"
+
+#class Tsoha < Sinatra::Base
+
+#  enable :sessions
+#  set :public, File.dirname(__FILE__) + "/public"
 
   get '/' do
-    @esimerkkimuuttuja = "tämä on muuttuja"
-    @sessiosta_muuttujaan = session[:muuttuja]
-    @testmodelin_arvot = User.all
+#    @esimerkkimuuttuja = "tämä on muuttuja"
+#    @sessiosta_muuttujaan = session[:muuttuja]
+#    @testmodelin_arvot = User.all
     erb :index
   end
-
-  get '/sessioon/:arvo' do
-    session[:muuttuja] = params[:arvo]
-    redirect '/'
+  
+  get '/login' do
+	erb :login
+  end
+  
+  get '/paikat' do
+	erb :paikat
+  end
+  
+  get '/register' do
+	erb :register
   end
 
-end
+#  get '/sessioon/:arvo' do
+#    session[:muuttuja] = params[:arvo]
+#    redirect '/'
+#  end
+
+#end
